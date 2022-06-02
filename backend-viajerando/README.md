c1f677133112823d2942009adc66b88221c28acb
Gabriel Borges <63378134+bielborgesc@users.noreply.github.com>
2022-06-02 19:58:47 -0300

    Delete backend-viajerando directory

--- backend-viajerando/README.md
+++
@@ -1,1 +0,0 @@
-# Viajerando-Project
--- backend-viajerando/viajerando-backend/demo/src/main/java/com/viajerando/demo/repository/DestinyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.viajerando.demo.repository;
-
-import com.viajerando.demo.entity.Destiny;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DestinyRepository extends JpaRepository<Destiny, Long> {}
