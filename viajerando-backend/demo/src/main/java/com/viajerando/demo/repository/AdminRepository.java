e35a3b99e3e0593fc57bd925e356dc510cf52a57
Gabriel Borges <63378134+bielborgesc@users.noreply.github.com>
2022-06-02 20:06:59 -0300

    Delete viajerando-backend directory

--- viajerando-backend/demo/src/main/java/com/viajerando/demo/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.viajerando.demo.repository;
-
-import com.viajerando.demo.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {}
