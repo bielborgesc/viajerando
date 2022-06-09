c83fd689e56efd8c9b65792674bbcfca487530a2
Jackson_almeida <dealmeida.jackson@gmail.com>
2022-06-08 23:41:29 -0300

    Correção cors

--- demo/src/main/java/com/viajerando/demo/security/JWTConfig.java
+++ demo/src/main/java/com/viajerando/demo/security/JWTConfig.java
@@ -37,15 +37,26 @@ public class JWTConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "*").permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "*").permitAll()
-                .antMatchers(HttpMethod.GET, "*").permitAll()
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/users").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/login").permitAll()
                 .anyRequest().authenticated()
-                .and()
+                .and().cors()
+                .and().csrf().disable()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTValidateFilter(authenticationManager()))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.csrf().disable().authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "*").permitAll()
+//                .antMatchers(HttpMethod.OPTIONS, "*").permitAll()
+//                .antMatchers(HttpMethod.GET, "*").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+//                .addFilter(new JWTValidateFilter(authenticationManager()))
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
 
